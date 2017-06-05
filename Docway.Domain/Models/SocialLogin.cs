db38590b85bd315f50b32bb24ab8398cc364590f
Carlos Bueno <carlos.bueno@usp.br>
2017-06-05 16:21:18 -0300

    Model v1

---
+++ Docway.Domain/Models/SocialLogin.cs
@@ -0,0 +1,18 @@
+﻿using Docway.Domain.Core.Models;
+using Docway.Domain.Enum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Docway.Domain.Models
+{
+    public class SocialLogin : Entity
+    {
+        public SocialNetworks SocialId { get; set; }
+        public string Token { get; set; }
+        public string PriflePhotoUrl { get; set; }
+        public string SocialUserId { get; set; }
+    }
+}
